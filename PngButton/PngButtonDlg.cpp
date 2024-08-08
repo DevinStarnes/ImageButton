94ba84bf9171b00ab6c74780fb40713ceda7a551
DevinStarnes <devinstarnes98765@gmail.com>
2024-08-09 01:47:15 +0900

    Recover repository

---
+++ PngButton/PngButtonDlg.cpp
@@ -0,0 +1,171 @@
+
+// PngButtonDlg.cpp : ���� ����
+//
+
+#include "stdafx.h"
+#include "PngButton.h"
+#include "PngButtonDlg.h"
+#include "afxdialogex.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// ���� ���α׷� ������ ���Ǵ� CAboutDlg ��ȭ �����Դϴ�.
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+
+// ��ȭ ���� �������Դϴ�.
+	enum { IDD = IDD_ABOUTBOX };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV �����Դϴ�.
+
+// �����Դϴ�.
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(CAboutDlg::IDD)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CPngButtonDlg ��ȭ ����
+
+
+
+CPngButtonDlg::CPngButtonDlg(CWnd* pParent /*=NULL*/)
+	: CDialogEx(CPngButtonDlg::IDD, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CPngButtonDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_BUTTON1, m_btn);
+}
+
+BEGIN_MESSAGE_MAP(CPngButtonDlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_WM_ERASEBKGND()
+END_MESSAGE_MAP()
+
+
+// CPngButtonDlg �޽��� ó����
+
+BOOL CPngButtonDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// �ý��� �޴��� "����..." �޴� �׸��� �߰��մϴ�.
+
+	// IDM_ABOUTBOX�� �ý��� ���� ������ �־�� �մϴ�.
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != NULL)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+		ASSERT(bNameValid);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	// �� ��ȭ ������ �������� �����մϴ�.  ���� ���α׷��� �� â�� ��ȭ ���ڰ� �ƴ� ��쿡��
+	//  �����ӿ�ũ�� �� �۾��� �ڵ����� �����մϴ�.
+	SetIcon(m_hIcon, TRUE);			// ū �������� �����մϴ�.
+	SetIcon(m_hIcon, FALSE);		// ���� �������� �����մϴ�.
+
+	// TODO: ���⿡ �߰� �ʱ�ȭ �۾��� �߰��մϴ�.
+	m_btn.SetImage(IDB_CLOSE, L"PNG");
+	m_btn.SetSize(20, 20);
+	m_btn.MoveWindow(0, 0, 20, 20); // move window
+
+	return TRUE;  // ��Ŀ���� ��Ʈ�ѿ� �������� ������ TRUE�� ��ȯ�մϴ�.
+}
+
+void CPngButtonDlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+// ��ȭ ���ڿ� �ּ�ȭ ���߸� �߰��� ��� �������� �׸�����
+//  �Ʒ� �ڵ尡 �ʿ��մϴ�.  ����/�� ���� ����ϴ� MFC ���� ���α׷��� ��쿡��
+//  �����ӿ�ũ���� �� �۾��� �ڵ����� �����մϴ�.
+
+void CPngButtonDlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // �׸��⸦ ���� ����̽� ���ؽ�Ʈ�Դϴ�.
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// Ŭ���̾�Ʈ �簢������ �������� ����� ����ϴ�.
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// �������� �׸��ϴ�.
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+// ����ڰ� �ּ�ȭ�� â�� ���� ���ȿ� Ŀ���� ǥ�õǵ��� �ý��ۿ���
+//  �� �Լ��� ȣ���մϴ�.
+HCURSOR CPngButtonDlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+
+
+BOOL CPngButtonDlg::OnEraseBkgnd(CDC* pDC)
+{
+	CRect rc;
+	GetClientRect(&rc);
+
+	CBrush white(RGB(255, 255, 255));
+	auto oldBrush = pDC->SelectObject(&white);
+	BOOL bRes = pDC->PatBlt(0, 0, rc.Width(), rc.Height(), PATCOPY);
+	pDC->SelectObject(oldBrush);
+
+	return bRes;
+}
